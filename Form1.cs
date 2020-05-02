52852cbf2b3d45f0747290ddac8775483ae5d471
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2020-05-01 21:09:14 -0600

    took away old comments

--- Form1.cs
+++ Form1.cs
@@ -7,45 +7,44 @@ namespace Math_Quiz
     public partial class Form1 : Form
     {
         Random randomizer = new Random();
-        // + 
+
         int addend1;
         int addend2;
-        // -
+        
         int minuend;
         int subtrahend;
-        // x
+       
         int multiplicand;
         int multiplier;
-        // %
+       
         int dividend;
         int divisor;
-        // timer
+        
         int timeLeft;
 
         public void StartTheQuiz()
         {
-            // Fill in the addition problem.
+         
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
             sum.Value = 0;
 
-            // Fill in the subtraction problem.
+            
             minuend = randomizer.Next(1, 101);
             subtrahend = randomizer.Next(1, minuend);
             minusLeftLabel.Text = minuend.ToString();
             minusRightLabel.Text = subtrahend.ToString();
             difference.Value = 0;
 
-            // Fill in the multiplication problem.
+        
             multiplicand = randomizer.Next(2, 11);
             multiplier = randomizer.Next(2, 11);
             timesLeftLabel.Text = multiplicand.ToString();
             timesRightLabel.Text = multiplier.ToString();
             product.Value = 0;
 
-            // Fill in the division problem.
             divisor = randomizer.Next(2, 11);
             int temporaryQuotient = randomizer.Next(2, 11);
             dividend = divisor * temporaryQuotient;
